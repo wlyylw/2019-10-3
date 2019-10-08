4ab7e6c123930a859781629c87a571fe896fd203
wlyylw <931316452@qq.com>
2019-10-08 18:17:06 +0800

    audio

--- actvoice/app/src/main/java/com/example/actvoice/MainActivity.java
+++ actvoice/app/src/main/java/com/example/actvoice/MainActivity.java
@@ -6,81 +6,212 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import jaygoo.widget.wlv.WaveLineView;
 
 public class MainActivity extends AppCompatActivity {
-    int flag = 0;
-    int first=0;
+
+    int flag_play=0;    //是否有录音
+    int flag_record=0;
     ImageButton button_start;
-    ImageButton button_stop;
     ImageButton button_play;
-    ImageButton button_play_stop;
     private File recordFile;
+    PlayRecord playRecord;
     WaveLineView waveLineView;
     private MediaRecorder mediaRecorder;
+    String RecordPath;
+    String RecordFileName;
+
+    public static final String SD_APP_DIR_NAME = "mnt"; //存储程序在外部SD卡上的根目录的名字
+    public static final String VOICE_DIR_NAME = "sdcard";    //存储音频在根目录下的文件夹名字
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recordFile = new File("/mnt/sdcard", "wlyylw.amr");
-       PermissionManagerRecord();
+        PermissionManagerRecord();//授权
+//        recordFile = new File("/mnt/sdcard", "a.amr");
+                recordFile = new File("/mnt/sdcard", "a.amr");
+        //TODO:
+//       createRecordFile();  //创建目录
        SetObj();
+        CreateRecorder();
        ListenerManager();
-    }
-
-    private void SetObj()
-    {
-        button_start = findViewById(R.id.start);
-        button_stop = findViewById(R.id.stop);
-        button_play =findViewById(R.id.start_play);
-        button_play_stop =findViewById(R.id.stop_play);
-        waveLineView =findViewById(R.id.waveLineView);
     }
     private void ListenerManager()
     {
+        playRecord = new PlayRecord(MainActivity.this);
+        //录音和暂停录音
         button_start.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onClick(View view) {
-                startRecord();
-                flag++;
-                first=1;
+                //切图逻辑
+                if(flag_record==0) { startRecording();flag_record=1; }
+                else { stopRecording();flag_record=0; }
+                if(flag_record==0) { button_start.setImageResource(R.drawable.ic_yellowstart); }
+                if(flag_record==1) { button_start.setImageResource(R.drawable.ic_yellowstop); }
             }
         });
-        button_stop.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View view) {
-                stopRecord();
-            }
-        });
+        //播放和停止播放
         button_play.setOnClickListener(new View.OnClickListener(){
-
             @Override
             public void onClick(View view) {
+                if(flag_play==0) { playRecord();flag_play=1; }
+                else { stopPlayRecord();flag_play=0; }
+//                if(flag_play==0) { button_play.setImageResource(R.drawable.ic_bluestart); }
+//                if(flag_play==1) { button_play.setImageResource(R.drawable.ic_bluepause); }
+
 
             }
         });
-        button_play_stop.setOnClickListener(new View.OnClickListener(){
 
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
     }
 
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    //开始录音
+    private void startRecording() {
+
+        mediaRecorder = new MediaRecorder();
+        // 判断，若当前文件已存在，则删除
+        if (recordFile.exists()) {
+            recordFile.delete();
+        }
+        //设定录音来源为主麦克风。
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
+        mediaRecorder.setOutputFile(recordFile.getAbsolutePath());
+        try {
+            // 准备好开始录音
+            mediaRecorder.prepare();
+            mediaRecorder.start();
+            waveLineView.startAnim();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Toast.makeText(this,"开始录音",Toast.LENGTH_SHORT).show();
+
+    }
+    //停止录音
+    private void stopRecording() {
+        Toast.makeText(this,"结束录音",Toast.LENGTH_SHORT).show();
+        if (recordFile != null) {
+            waveLineView.onPause();
+            mediaRecorder.stop();
+            mediaRecorder.release();
+        }
+        //TODO:
+//        saveVoiceToSD(RecordPath);
+    }
+
+
+    //开始播放
+    private void playRecord()
+    {
+        playRecord.playRecordFile(recordFile);
+        waveLineView.startAnim();
+    }
+    //停止播放
+    private void stopPlayRecord()
+    {
+        playRecord.stopRecordFile();
+        waveLineView.onPause();
+    }
+
+
+
+    private void saveVoiceToSD(String path) {
+        //创建输入输出
+        InputStream isFrom = null;
+        OutputStream osTo = null;
+        try {
+            //设置输入输出流
+            isFrom = new FileInputStream(path);
+            osTo = new FileOutputStream(RecordPath);
+            byte bt[] = new byte[1024];
+            int len;
+            while ((len = isFrom.read(bt)) != -1) {
+                Log.d("storeRec", "len = " + len);
+                osTo.write(bt, 0, len);
+            }
+            Log.d("storeRec", "保存录音完成。");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (osTo != null) {
+                try {
+                    //不管是否出现异常，都要关闭流
+                    osTo.close();
+                    Log.d("storeRec", "关闭输出流");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (isFrom != null) {
+                try {
+                    isFrom.close();
+                    Log.d("storeRec", "关闭输入流");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+
+    //创建音频目录
+    private void createRecordFile()
+    {
+        RecordFileName = getStoreTime()+".amr";
+        //用于存储录音的最终目录，即根目录 / 录音的文件夹 / 录音
+        recordFile = new File(Environment.getExternalStorageDirectory().getAbsolutePath()
+            + "/" + SD_APP_DIR_NAME + "/" + VOICE_DIR_NAME + "/", RecordFileName);
+        //TODO:
+//        recordFile = new File("/" + SD_APP_DIR_NAME + "/" + VOICE_DIR_NAME + "/", RecordFileName);
+        RecordPath =recordFile.getAbsolutePath();
+
+        recordFile.getParentFile().mkdir();
+        recordFile.setWritable(true);
+
+    }
+
+    //初始化
+    private void SetObj()
+    {
+        button_start = findViewById(R.id.start);
+        button_play =findViewById(R.id.start_play);
+        waveLineView =findViewById(R.id.waveLineView);
+    }
+    //权限
     private void PermissionManagerRecord()
     {
         if(ContextCompat.checkSelfPermission
@@ -102,70 +233,24 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this, new String[]
                     {Manifest.permission.WAKE_LOCK}, 200);
     }
-
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    private void startRecord() {
+    //用时间命名文件
+    private String getStoreTime()
+    {
+        String time ;
+        String ampTime;
+        int apm = Calendar.getInstance().get(Calendar.AM_PM);
+        ampTime = apm==0? "am":"pm";
+        SimpleDateFormat format = new SimpleDateFormat("yyyy_MM_dd E " + ampTime + " kk:mm:ss");
+        time = format.format(new Date());
+        return time;
+    }
+    //录音参数设置
+    public void CreateRecorder()
+    {
         mediaRecorder = new MediaRecorder();
         mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC); //设定录音来源为主麦克风。
         mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
         mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
         mediaRecorder.setOutputFile(recordFile.getAbsolutePath());
-
-        if(recordFile.exists())
-        {
-            recordFile.delete();
-        }
-        if (flag % 2 == 0) {
-            button_start.setImageResource(R.drawable.ic_yellowstart);
-            if (first == 0)
-            {
-            try {
-                // 准备好开始录音
-                mediaRecorder.prepare();
-                mediaRecorder.start();
-                waveLineView.startAnim();
-
-            } catch (IllegalStateException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            }
-            else
-            {
-
-                    // 准备好开始录音
-//                    mediaRecorder.prepare();
-//                    mediaRecorder.start();
-                    waveLineView.onResume();
-            }
-        }
-
-        else
-        {
-
-            button_start.setImageResource(R.drawable.ic_yellowstop);
-            waveLineView.onPause();
-            try {
-                // 准备好开始录音
-//                mediaRecorder.stop();
-                //TODO:拼接两段录音
-            } catch (IllegalStateException e) {
-                e.printStackTrace();
-            }
-        }
-
-
     }
-    private void stopRecord()
-    {
-        if (recordFile != null) {
-
-            mediaRecorder.stop();
-            mediaRecorder.release();
-//            waveLineView.onPause();
-            waveLineView.stopAnim();
-        }
-    }
-
 }
