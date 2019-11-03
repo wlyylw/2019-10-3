6c0351935e3c707c23209844acecc39ebbf31282
wlyylw <931316452@qq.com>
2019-11-03 15:59:31 +0800

    add

--- StudyList/app/src/main/java/com/example/studylist/MainActivity.java
+++ StudyList/app/src/main/java/com/example/studylist/MainActivity.java
@@ -12,11 +12,22 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     MinePageService minePageService;
+
+    MinePageDynamicService minePageDynamicService;
+    RecyclerView recyclerViewDynamic;
+
+    MinePagePersonService minePagePersonService;
+    RecyclerView recyclerViewPerson;
+
     private List<MinePageInformation> list = new ArrayList<>();
+    private List<MineDynamic> listMineDynamic = new ArrayList<>();
+    private List<MinePagePerson> listMinePerson = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Information
         minePageService = MinePageService.getInstance();
         list = minePageService.getMineService();
         recyclerView = findViewById(R.id.recycler_view_three);
@@ -24,5 +35,27 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         MinePageAdapter adapter = new MinePageAdapter(list);
         recyclerView.setAdapter(adapter);
+
+        //Dynamic
+        minePageDynamicService = MinePageDynamicService.getInstance();
+        listMineDynamic = minePageDynamicService.getList();
+        recyclerViewDynamic = findViewById(R.id.recycler_view_mine_dynamic);
+        LinearLayoutManager layoutManagerDynamic = new LinearLayoutManager(this);
+        layoutManagerDynamic.setOrientation(LinearLayoutManager.HORIZONTAL);
+        recyclerViewDynamic.setLayoutManager(layoutManagerDynamic);
+        MinePageDynamicAdapter minePageDynamicAdapter =new MinePageDynamicAdapter(listMineDynamic);
+        recyclerViewDynamic.setAdapter(minePageDynamicAdapter);
+
+        //Person
+        minePagePersonService = MinePagePersonService.getInstance();
+        listMinePerson = minePagePersonService.getList();
+        recyclerViewPerson = findViewById(R.id.recycler_view_mine_person);
+        RecyclerView.LayoutManager layoutManagerPerson = new LinearLayoutManager(this);
+        recyclerViewPerson.setLayoutManager(layoutManagerPerson);
+        MinePagePersonAdapter minePagePersonAdapter = new MinePagePersonAdapter(listMinePerson);
+        recyclerViewPerson.setAdapter(minePagePersonAdapter);
+
+
+
     }
 }
---
+++ StudyList/app/src/main/java/com/example/studylist/MinePagePersonService.java
@@ -0,0 +1,31 @@
+package com.example.studylist;
+
+import java.util.ArrayList;
+
+public class MinePagePersonService {
+    ArrayList<MinePagePerson> list;
+
+    private static  MinePagePersonService instance;
+
+    public static MinePagePersonService getInstance() {
+        if(instance==null)
+            instance = new MinePagePersonService();
+        return  instance;
+    }
+
+    public ArrayList<MinePagePerson> getList(){
+        return list;
+    }
+
+    private  MinePagePersonService()
+    {
+        list = new ArrayList<>();
+        InitEntity();
+    }
+
+    private void InitEntity() {
+        MinePagePerson minePagePerson = new MinePagePerson("五月她如景");
+        list.add(minePagePerson);
+
+    }
+}
